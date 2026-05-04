3d5e7e9914eb7626e5cebb6998531dae9c962d79
limanp <pasha.liman@ukr.net>
2026-05-04 22:31:37 +0300

    update

--- Messenger/MainWindow.xaml.cs
+++ Messenger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,24 +19,49 @@ namespace Messenger
     
     public partial class MainWindow : Window
     {
+        ChatViewModel chatViewModel = new ChatViewModel();
         public MainWindow()
         {
             InitializeComponent();
-
-            List<MessageData> items = new List<MessageData>();
-            items.Add(new MessageData() { ContactName = "Pavlo", Text = "Hello!" });
-            items.Add(new MessageData() { ContactName = "James", Text = "Hello, How are you?", Sender = MessageSender.Contact});
-            items.Add(new MessageData() { ContactName = "Pavlo", Text = "Nice, and you?" });
-          
-            icMessageList.ItemsSource = items;
         }
-        internal void InitializeMesseging()
+     
+
+        private void Send_Message(object sender, RoutedEventArgs e)
         {
+            var text = MessageTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                MessageData message = new MessageData() { ContactName = "Pavlo", Text = text };
+                chatViewModel.Messages.Add(message); 
+             
+                MessageTextBox.Clear();
+            }
             
         }
-
     }
-   
+
+    public class ChatViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MessageData> Messages { get; set; } = new ObservableCollection<MessageData>();
+
+        private object _currentChat;
+        public object CurrentChat
+        {
+            get => _currentChat;
+            set { _currentChat = value; OnPropertyChanged(); }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged; 
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+
+        //internal void SelectContact(Contact contact)
+        //{
+        //    CurrentChat = Chat(contact);
+        //}
+        
+    }
+
     internal enum MessageSender
     {
         Me, 
@@ -51,5 +77,9 @@ namespace Messenger
         public bool IsFromMe => Sender == MessageSender.Me; 
         
     }
-
+    internal class Chat
+    {
+        public List<MessageData> messages { get; set; } 
+        public int messageAmount { get; set; }  
+    }
 }
